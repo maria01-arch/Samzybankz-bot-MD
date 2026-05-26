b347cff179b4a19008572bf3dba9e499baea2800
maria01-arch <maria01olivia1@gmail.com>
2026-05-26 01:53:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "samzyXMD Bot",
+  botOwner: 'Professor', // samzy bankz 
+  ownerNumber: '919876543210', //2347064246567,
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
